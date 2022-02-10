b1eb8ddaa7f1526502925aedcf9ed8e168b00995
locdaile <ldl@exo.consulting>
2022-02-10 15:52:34 +0100

    minor

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,21 @@
-alert("This alert box was called with the onload event");
-
 const params = (new URL(location)).searchParams;
 
 var code = params.get('code');
 
-alert(code);
+
+
+var formData = new FormData();
+formData.append("code", code);
+
+$.ajax({
+    'method': 'post',
+    'url': 'https://prod-247.westeurope.logic.azure.com:443/workflows/a3a93cf5cd6b4a489924ea40328e961c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VwIdgsPc3w40Q0Y0UE3UsoIce1gZcq-nlaA4z8rciFM',
+    'data': formData
+}).then((result) => {
+    if (result.success) {
+        alert(code);
+    }
+    else {
+        alert("This alert box was called with the onload event");
+    }
+});
