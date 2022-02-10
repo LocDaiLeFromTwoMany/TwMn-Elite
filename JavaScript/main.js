0d8bdc6968516c6eac7b6239de160b0da3072698
locdaile <ldl@exo.consulting>
2022-02-10 16:16:08 +0100

    minor

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,14 +1,14 @@
 const params = (new URL(location)).searchParams;
 
-var code = params.get('code');
+var stravaCode = params.get('code');
 
-var formData = new FormData();
-formData.append("code", code);
+
+var obj = {code: stravaCode};
 
 $.ajax({
     method: 'post',
     url: 'https://prod-247.westeurope.logic.azure.com:443/workflows/a3a93cf5cd6b4a489924ea40328e961c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VwIdgsPc3w40Q0Y0UE3UsoIce1gZcq-nlaA4z8rciFM',
-    data: formData,
+    data: JSON.stringify(obj),
     processData: false,
     contentType: false,
     success: function() {
