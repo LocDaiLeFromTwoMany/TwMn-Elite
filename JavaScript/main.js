7873103ac2536e25054220b3f8181061ecd4d291
locdaile <ldl@exo.consulting>
2022-02-10 15:55:52 +0100

    Minor changes

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,15 +2,15 @@ const params = (new URL(location)).searchParams;
 
 var code = params.get('code');
 
-
-
 var formData = new FormData();
 formData.append("code", code);
 
 $.ajax({
-    'method': 'post',
-    'url': 'https://prod-247.westeurope.logic.azure.com:443/workflows/a3a93cf5cd6b4a489924ea40328e961c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VwIdgsPc3w40Q0Y0UE3UsoIce1gZcq-nlaA4z8rciFM',
-    'data': formData
+    method: 'post',
+    url: 'https://prod-247.westeurope.logic.azure.com:443/workflows/a3a93cf5cd6b4a489924ea40328e961c/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VwIdgsPc3w40Q0Y0UE3UsoIce1gZcq-nlaA4z8rciFM',
+    data: formData,
+    processData: false,
+    contentType: false,
 }).then((result) => {
     if (result.success) {
         alert(code);
